baa3efd5a715081b84a1c389c6560b909525989e
al-burlachenko <alexandrburl@gmail.com>
2026-01-03 13:57:11 +0200

    adds api functions

---
+++ src/js/utils/apiService.js
@@ -0,0 +1,181 @@
+import axios from "axios";
+
+const API_KEY = import.meta.env.VITE_API_KEY;
+
+axios.defaults.baseURL = "https://api.themoviedb.org/3";
+axios.defaults.headers = {
+  accept: "application/json",
+  Authorization: `Bearer ${API_KEY}`,
+};
+
+// export function fetchMovie(query = "tron") {
+//   axios
+//     .get("authentication")
+//     .then((res) => console.log(res.data))
+//     .catch((e) => console.log("error", e));
+// }
+
+// запит одного фільма
+
+// Get collection details by ID. https://api.themoviedb.org/3/collection/{collection_id}
+// Get the images that belong to a collection. https://api.themoviedb.org/3/collection/{collection_id}/images
+// Translations https://api.themoviedb.org/3/collection/{collection_id}/translations
+
+function getMoviesByFilters(params) {
+  // Find movies using over 30 filters and sort options.
+  // main searching request for compilations i guess
+  console.log(params, "params here i dunno what this is advanced search");
+
+  return axios
+    .get("discover/movie")
+    .then((res) => res.data)
+    .catch((e) => console.log(e));
+  // Advanced search using over 30 filters and sort options.
+  // certification
+  // include_adult
+  // include_video
+  // language
+  // page
+  // primary_release_year
+  // region
+  // release_date
+  // vote_average
+  // vote_count
+  // watch_region
+  // with_cast
+  // with_companies
+  // with_crew
+  // with_genres
+  // with_keywords
+  // with_origin_country
+  // with_original_language
+  // with_people
+  // with_release_type
+  // with_runtime
+  // with_watch_monetization_types
+  // with_watch_providers
+  // without_companies
+  // without_genres
+  // without_keywords
+  // without_watch_providers
+  // year
+}
+// similar for tv https://api.themoviedb.org/3/discover/tv
+
+function getGenresList() {
+  // Get the list of official genres for movies.
+  return axios.get("genre/movie/list").then((res) => res.data);
+}
+
+// MOVIE LISTS:
+function getNowPlayingMoviesList(lang = "en-US", page = 1, region) {
+  return axios
+    .get("movie/now_playing")
+    .then((res) => res.data)
+    .catch((e) => console.log(e));
+  // Get a list of movies that are currently in theatres.
+}
+function getPopularMoviesList(lang = "en-US", page = 1, region) {
+  return axios
+    .get("movie/popular")
+    .then((res) => res.data)
+    .catch((e) => console.log(e));
+  // Get a list of movies ordered by popularity.
+}
+function getTopRatedMoviesList(lang = "en-US", page = 1, region) {
+  return axios
+    .get("movie/top_rated")
+    .then((res) => res.data)
+    .catch((e) => console.log(e));
+  // Get a list of movies ordered by rating.
+}
+function getUpcomingMoviesList(lang = "en-US", page = 1, region) {
+  return axios
+    .get("movie/upcoming")
+    .then((res) => res.data)
+    .catch((e) => console.log(e));
+  // Get a list of movies that are being released soon.
+}
+
+function getTMDBTrendingByDayMoviesList(lang = "en-US") {
+  return axios
+    .get(`trending/movie/day?language=${lang}`)
+    .then((res) => res.data)
+    .catch((e) => console.log(e));
+  // Get the trending movies on TMDB.
+  // time_window required
+  // Allowed: day week
+}
+
+function getTMDBTrendingByWeekMoviesList(lang = "en-US") {
+  return axios
+    .get(`trending/movie/week?language=${lang}`)
+    .then((res) => res.data)
+    .catch((e) => console.log(e));
+  // same
+}
+
+function getKeyWordTitleById(keyword_id) {
+  return axios
+    .get(`keyword/${keyword_id}`)
+    .then((res) => res.data)
+    .catch((e) => console.log(e));
+}
+
+function getMovieById() {
+  // Get the top level details of a movie by ID.
+  return axios
+    .get(`movie/${movie_id}`)
+    .then((res) => res.data)
+    .catch((e) => console.log(e));
+}
+
+function getKeywordIdByTitle(query, page) {
+  // Search for keywords by their name.
+  return axios
+    .get(`search/keyword?query=${query}&page=${page}`)
+    .then((res) => res.data)
+    .catch((e) => console.log(e));
+}
+
+function getMovieByTitle(query, page = 1) {
+  // query required
+  // include_adult boolean
+  // language  Defaults to en-US
+  // primary_release_year
+  // page
+  // region
+  // year
+  return axios
+    .get(
+      `search/movie?query=${query}&include_adult=false&language=en-US&page=${page}`
+    )
+    .then((res) => res.data)
+    .catch((e) => console.log(e));
+}
+
+// Get the rating, watchlist and favourite status of an account. https://api.themoviedb.org/3/movie/{movie_id}/account_states
+// Get the alternative titles for a movie. https://api.themoviedb.org/3/movie/{movie_id}/alternative_titles
+// Get the recent changes for a movie. https://api.themoviedb.org/3/movie/{movie_id}/changes
+// https://api.themoviedb.org/3/movie/{movie_id}/credits
+// Get the images that belong to a movie https://api.themoviedb.org/3/movie/{movie_id}/images
+// https://api.themoviedb.org/3/movie/{movie_id}/keywords
+// https://api.themoviedb.org/3/movie/{movie_id}/recommendations
+// https://api.themoviedb.org/3/movie/{movie_id}/similar
+// https://api.themoviedb.org/3/movie/{movie_id}/translations
+// https://api.themoviedb.org/3/movie/{movie_id}/videos => https://www.youtube.com/watch?v=${key}!
+
+export {
+  getMoviesByFilters,
+  getGenresList,
+  getNowPlayingMoviesList,
+  getPopularMoviesList,
+  getTopRatedMoviesList,
+  getUpcomingMoviesList,
+  getTMDBTrendingByDayMoviesList,
+  getTMDBTrendingByWeekMoviesList,
+  getKeyWordTitleById,
+  getMovieById,
+  getKeywordIdByTitle,
+  getMovieByTitle,
+};
--- src/main.js
+++ src/main.js
@@ -1,24 +1,114 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import {
+  getMoviesByFilters,
+  getGenresList,
+  getNowPlayingMoviesList,
+  getPopularMoviesList,
+  getTopRatedMoviesList,
+  getUpcomingMoviesList,
+  getTMDBTrendingByDayMoviesList,
+  getTMDBTrendingByWeekMoviesList,
+  getKeyWordTitleById,
+  getMovieById,
+  getKeywordIdByTitle,
+  getMovieByTitle,
+} from "./js/utils/apiService";
+import "./style.css";
 
-document.querySelector('#app').innerHTML = `
+document.querySelector("#app").innerHTML = `
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
+   
     <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
 
-setupCounter(document.querySelector('#counter'))
+    <p class="read-the-docs">
+      lorem ipso eto vsyo ipso
+    </p>
+
+<button class="getMoviesByFilters">getMoviesByFilters</button>
+<button class="getGenresList">getGenresList</button>
+<button class="getNowPlayingMoviesList">getNowPlayingMoviesList</button>
+<button class="getPopularMoviesList">getPopularMoviesList</button>
+<button class="getTopRatedMoviesList">getTopRatedMoviesList</button>
+<button class="getUpcomingMoviesList">getUpcomingMoviesList</button>
+<button class="getTMDBTrendingByDayMoviesList">getTMDBTrendingByDayMoviesList</button>
+<button class="getTMDBTrendingByWeekMoviesList">getTMDBTrendingByWeekMoviesList</button>
+<button class="getKeyWordTitleById">getKeyWordTitleById</button>
+<button class="getMovieById">getMovieById</button>
+<button class="getKeywordIdByTitle">getKeywordIdByTitle</button>
+<button class="getMovieByTitle">getMovieByTitle</button>
+
+  </div>
+`;
+
+function onBtnClick(e) {
+  console.log(e.target);
+  fetchMovie();
+}
+
+const buttons = {
+  getMoviesByFilters: document.querySelector(".getMoviesByFilters"),
+  getGenresList: document.querySelector(".getGenresList"),
+  getNowPlayingMoviesList: document.querySelector(".getNowPlayingMoviesList"),
+  getPopularMoviesList: document.querySelector(".getPopularMoviesList"),
+  getTopRatedMoviesList: document.querySelector(".getTopRatedMoviesList"),
+  getUpcomingMoviesList: document.querySelector(".getUpcomingMoviesList"),
+  getTMDBTrendingByDayMoviesList: document.querySelector(
+    ".getTMDBTrendingByDayMoviesList"
+  ),
+  getTMDBTrendingByWeekMoviesList: document.querySelector(
+    ".getTMDBTrendingByWeekMoviesList"
+  ),
+  getKeyWordTitleById: document.querySelector(".getKeyWordTitleById"),
+  getMovieById: document.querySelector(".getMovieById"),
+  getKeywordIdByTitle: document.querySelector(".getKeywordIdByTitle"),
+  getMovieByTitle: document.querySelector(".getMovieByTitle"),
+};
+
+buttons.getMoviesByFilters.addEventListener("click", (e) => {
+  getMoviesByFilters().then((res) => console.log(res));
+});
+
+buttons.getGenresList.addEventListener("click", (e) => {
+  getGenresList().then((res) => console.log(res));
+});
+
+buttons.getNowPlayingMoviesList.addEventListener("click", (e) => {
+  getNowPlayingMoviesList().then((res) => console.log(res));
+});
+
+buttons.getPopularMoviesList.addEventListener("click", (e) => {
+  getPopularMoviesList().then((res) => console.log(res));
+});
+
+buttons.getTopRatedMoviesList.addEventListener("click", (e) => {
+  getTopRatedMoviesList().then((res) => console.log(res));
+});
+
+buttons.getUpcomingMoviesList.addEventListener("click", (e) => {
+  getUpcomingMoviesList().then((res) => console.log(res));
+});
+
+buttons.getTMDBTrendingByDayMoviesList.addEventListener("click", (e) => {
+  getTMDBTrendingByDayMoviesList().then((res) => console.log(res));
+});
+
+buttons.getTMDBTrendingByWeekMoviesList.addEventListener("click", (e) => {
+  getTMDBTrendingByWeekMoviesList().then((res) => console.log(res));
+});
+
+buttons.getKeyWordTitleById.addEventListener("click", (e) => {
+  getKeyWordTitleById().then((res) => console.log(res));
+});
+
+buttons.getMovieById.addEventListener("click", (e) => {
+  getMovieById().then((res) => console.log(res));
+});
+
+buttons.getKeywordIdByTitle.addEventListener("click", (e) => {
+  getKeywordIdByTitle().then((res) => console.log(res));
+});
+
+buttons.getMovieByTitle.addEventListener("click", (e) => {
+  getMovieByTitle("tron").then((res) => console.log(res));
+});
+
+// getMoviesByFilters getKeyWordTitleById getMovieById getKeywordIdByTitle getMovieByTitle !! check
